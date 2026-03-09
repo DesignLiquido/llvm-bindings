12a6e1a90df101fbe59e7223507fe997edd8c8dd
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-03-08 23:08:36 -0700

    Additional fixes for Ubuntu.

--- cmake/LLVM.cmake
+++ cmake/LLVM.cmake
@@ -7,14 +7,18 @@ if (CMAKE_HOST_APPLE)
     endforeach ()
 endif ()
 
-if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
-    # Ubuntu apt-installed LLVM packages place cmake files under /usr/lib/llvm-X/cmake/
-    # Enumerate installed versions so CMake can find them regardless of version compat quirks
-    file(GLOB LLVM_DIRS_LINUX /usr/lib/llvm-*)
-    foreach (LLVM_DIR ${LLVM_DIRS_LINUX})
-        list(APPEND CMAKE_PREFIX_PATH ${LLVM_DIR}/cmake)
-        list(APPEND CMAKE_PREFIX_PATH ${LLVM_DIR}/lib/cmake/llvm)
-    endforeach ()
+if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux" AND NOT DEFINED LLVM_DIR)
+    # Ubuntu apt installs LLVM cmake config at /usr/lib/llvm-X/cmake/ (not in the default
+    # CMake search path). Multiple versions may be installed; find the highest one >= 18.
+    foreach(llvm_major_ver 30 29 28 27 26 25 24 23 22 21 20 19 18)
+        if (EXISTS "/usr/lib/llvm-${llvm_major_ver}/cmake/LLVMConfig.cmake")
+            set(LLVM_DIR "/usr/lib/llvm-${llvm_major_ver}/cmake" CACHE PATH "" FORCE)
+            break()
+        elseif (EXISTS "/usr/lib/llvm-${llvm_major_ver}/lib/cmake/llvm/LLVMConfig.cmake")
+            set(LLVM_DIR "/usr/lib/llvm-${llvm_major_ver}/lib/cmake/llvm" CACHE PATH "" FORCE)
+            break()
+        endif()
+    endforeach()
 endif ()
 
 # Do not pass a version to find_package: Ubuntu's apt LLVM cmake config files use strict
